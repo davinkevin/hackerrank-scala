cc21e8d82c909f43ab932c4be0795348c7f43388
davinkevin <davin.kevin@gmail.com>
2017-10-09 05:26:39 +0200

    feat(A Very Big Sum): solution of problem
    Problem from https://www.hackerrank.com/challenges/a-very-big-sum/problem

---
+++ src/main/scala/averybigsum/Solution.scala
@@ -0,0 +1,19 @@
+package averybigsum
+
+import scala.io.Source
+
+/**
+  * Created by kevin on 08/10/2017
+  */
+object Solution {
+
+  def main(args: Array[String]): Unit = {
+    val sum = Source.stdin.getLines().slice(1, 2)
+      .flatMap(v => v.split(" ").toList)
+      .map(v => BigInt(v))
+      .sum
+
+    println(sum)
+  }
+
+}
