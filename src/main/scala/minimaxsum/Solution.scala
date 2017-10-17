a8133ed9ee8e201418f26b7f958e5839021f9e5c
davinkevin <davin.kevin@gmail.com>
2017-10-17 05:51:14 +0200

    feat(Mini-Max Sum): solution of problem
    Problem from https://www.hackerrank.com/challenges/mini-max-sum/problem

---
+++ src/main/scala/minimaxsum/Solution.scala
@@ -0,0 +1,25 @@
+package minimaxsum
+
+import scala.io.Source
+
+/**
+  * Created by kevin on 14/10/2017
+  */
+object Solution {
+  def main(args: Array[String]): Unit = {
+    Source.stdin.getLines().foreach(v => compute(v))
+    //compute("1 2 3 4 5")
+  }
+
+  def compute(str: String): Unit = {
+    val numbers = str.split(" ")
+      .toList
+      .map(v => BigInt(v))
+
+    val global = numbers.sum
+    val maxSum = global - numbers.min
+    val minSum = global - numbers.max
+
+    print(minSum + " " + maxSum)
+  }
+}
